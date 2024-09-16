38a5ea3b99d675a363327876083172c307c4fea6
MINEDUCYT <MINEDUCYT@S06-15>
2024-09-16 16:40:25 -0600

    Agregar archivos de proyecto.

---
+++ TheKisCoffee/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TheKisCoffee
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TheKisCoffee/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TheKisCoffee
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
